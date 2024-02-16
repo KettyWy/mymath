d33d153018e36922cb74521ae3222d52ed42db16
Екатерина <kettywy@mail.ru>
2024-02-16 22:44:35 +0300

    add funcs and tag v1.0.0

--- mymath.go
+++ mymath.go
@@ -8,6 +8,14 @@ func Sqrt(x float64) float64 {
 	return math.Sqrt(x)
 }
 
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+
+func Yn(x int, y float64) float64 {
+	return math.Yn(x, y)
+}
+
 func Ceil(x float64) float64 {
 	return math.Ceil(x)
 }
@@ -22,4 +30,14 @@ func Pow(x, y float64) float64 {
 
 func Max(x, y float64) float64 {
 	return math.Max(x, y)
+}
+
+func Acos(x float64) float64 {
+	return math.Acos(x)
+}
+func Acosh(x float64) float64 {
+	return math.Acosh(x)
+}
+func Asin(x float64) float64 {
+	return math.Asin(x)
 }
