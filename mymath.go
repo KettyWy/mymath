7d3636a0c7ab0a06acdbdb1fce27cc6ffe8132b3
Екатерина <kettywy@mail.ru>
2024-02-16 23:20:33 +0300

    fix

---
+++ mymath.go
@@ -0,0 +1,43 @@
+package mymath
+
+import (
+	"math"
+)
+
+func Sqrt(x float64) float64 {
+	return math.Sqrt(x)
+}
+
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+
+func Yn(x int, y float64) float64 {
+	return math.Yn(x, y)
+}
+
+func Ceil(x float64) float64 {
+	return math.Ceil(x)
+}
+
+func Floor(x float64) float64 {
+	return math.Floor(x)
+}
+
+func Pow(x, y float64) float64 {
+	return math.Pow(x, y)
+}
+
+func Max(x, y float64) float64 {
+	return math.Max(x, y)
+}
+
+func Acos(x float64) float64 {
+	return math.Acos(x)
+}
+func Acosh(x float64) float64 {
+	return math.Acosh(x)
+}
+func Asin(x float64) float64 {
+	return math.Asin(x)
+}
